94e361535ba079048c2aafeb78f714afd9c00b3f
bargaouitarek-gif <bargaouitarek@gmail.com>
2026-04-05 13:53:35 +0200

    Add files via upload

---
+++ page.tsx
@@ -0,0 +1,9 @@
+
+export default function Home() {
+  return (
+    <main style={{fontFamily:"Arial",padding:40}}>
+      <h1>X3BZ Application</h1>
+      <p>L'application est correctement installée.</p>
+    </main>
+  )
+}
