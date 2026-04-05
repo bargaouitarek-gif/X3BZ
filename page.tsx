57da137c198f24aa628b6db4a6d93fa913d38aab
bargaouitarek-gif <bargaouitarek@gmail.com>
2026-04-05 14:10:12 +0200

    Delete page.tsx

--- page.tsx
+++
@@ -1,9 +0,0 @@
-
-export default function Home() {
-  return (
-    <main style={{fontFamily:"Arial",padding:40}}>
-      <h1>X3BZ Application</h1>
-      <p>L'application est correctement installée.</p>
-    </main>
-  )
-}
